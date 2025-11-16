3f75f44f9ab7cdd4caf14900187c60d3a6f303be
Yako-Ken <yako0ae@gmail.com>
2025-11-16 10:43:11 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ function HomePage() {
         <ProductList/>
       </div>
       <div className="mt-24">
-        <h1 className="text-2xl ms-64">Categories</h1>
+        <h1 className="text-2xl ms-4 lg:ms-64">Categories</h1>
         <CategoryList/>
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
