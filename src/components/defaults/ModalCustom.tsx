bbdd5ced64c32f5bd54239ed33c63d4233ab3519
Yako-Ken <yako0ae@gmail.com>
2025-11-23 00:36:06 +0200

    Update ModalCustom.tsx

--- src/components/defaults/ModalCustom.tsx
+++ src/components/defaults/ModalCustom.tsx
@@ -1,21 +1,34 @@
-import { Button } from "@/app/components/ui/button";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogFooter,
-  DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/app/components/ui/dialog";
-import { Input } from "@/app/components/ui/input";
-import { Label } from "@/app/components/ui/label";
 import { ReactNode } from "react";
-const ModalCustom = ({ content, btn }: { content: ReactNode; btn: ReactNode }) => {
+
+interface ModalCustomProps {
+  title?: string;
+  description?: string;
+  content: ReactNode;
+  btn: ReactNode;
+  isMenuItem?: boolean;
+}
+
+const ModalCustom = ({
+  title,
+  description,
+  content,
+  btn,
+}: ModalCustomProps) => {
   return (
     <Dialog>
       <DialogTrigger asChild>{btn}</DialogTrigger>
-      <DialogContent className="sm:max-w-[425px] bg-white">{content}</DialogContent>
+      <DialogContent className="sm:max-w-[425px] bg-white">
+        {title && <DialogTitle>{title}</DialogTitle>}
+        {description && <DialogDescription>{description}</DialogDescription>}
+        {content}
+      </DialogContent>
     </Dialog>
   );
 };
