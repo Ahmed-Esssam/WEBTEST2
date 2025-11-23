7518d346cadbe86a8f7ae0957e5524d8a2dbccfe
Yako-Ken <yako0ae@gmail.com>
2025-11-23 08:46:13 +0200

    Update ProtectedRoute.tsx

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -2,10 +2,6 @@
 
 import React from "react";
 
-/**
- * ProtectedRoute مؤقتًا بدون تحقق من تسجيل الدخول
- * سيظهر أي محتوى داخل الأطفال مباشرة بدون إعادة التوجيه
- */
 export default function ProtectedRoute({
   children,
 }: {
